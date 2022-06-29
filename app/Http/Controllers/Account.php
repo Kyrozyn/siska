d08ffc08b938165252cadafdf2c5e44b81429b51
Andreas <andreasrr998@gmail.com>
2022-06-30 05:20:28 +0700

    captain definition

--- app/Http/Controllers/Account.php
+++ app/Http/Controllers/Account.php
@@ -15,4 +15,21 @@ class Account extends Controller
     {
         return view('account.login');
     }
+
+    public function loginaction(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+        $user = \App\Models\User::where('email', $email)->first();
+        if ($user) {
+            if (\Hash::check($password, $user->password)) {
+                \Auth::login($user);
+                return redirect('/dashboard');
+            } else {
+                return redirect('/login')->with('error', 'Password salah');
+            }
+        } else {
+            return redirect('/login')->with('error', 'Email tidak ditemukan');
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\Account::class, 'gettingStarted']);
 Route::get('/login', [\App\Http\Controllers\Account::class, 'login']);
+Route::post('/login', [\App\Http\Controllers\Account::class, 'loginaction']);
 
 Route::get('/dashboard', [\App\Http\Controllers\Dashboard::class, 'home']);
 Route::get('/dashboard/settings', [\App\Http\Controllers\Dashboard::class, 'settings']);
