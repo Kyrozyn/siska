e832fe09d1a541622792cca146833e826d47cbd4
Andreas <andreasrr998@gmail.com>
2022-07-21 09:17:05 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -17,31 +17,36 @@ Route::get('/', [\App\Http\Controllers\Account::class, 'gettingStarted']);
 Route::get('/login', [\App\Http\Controllers\Account::class, 'login']);
 Route::post('/login', [\App\Http\Controllers\Account::class, 'loginaction']);
 
-Route::get('/dashboard', [\App\Http\Controllers\Dashboard::class, 'home']);
-Route::get('/dashboard/settings', [\App\Http\Controllers\Dashboard::class, 'settings']);
 
-Route::get('/logout',[\App\Http\Controllers\Account::class, 'logout']);
+//route group middleware login
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/dashboard', [\App\Http\Controllers\Dashboard::class, 'home']);
+    Route::get('/dashboard/settings', [\App\Http\Controllers\Dashboard::class, 'settings']);
+
+    Route::get('/logout',[\App\Http\Controllers\Account::class, 'logout']);
 //topiktemuan
-Route::resource('/dashboard/topiktemuan', \App\Http\Controllers\TopikTemuan::class);
+    Route::resource('/dashboard/topiktemuan', \App\Http\Controllers\TopikTemuan::class);
 //viewmenutemuan
-Route::get('/dashboard/menutemuan/{topik_temuan_id}', [\App\Http\Controllers\MenuTemuan::class, 'index']);
-Route::get('/dashboard/menutemuan/{topik_temuan_id}/menutemuanid/{menu_temuan_id}', [\App\Http\Controllers\MenuTemuan::class, 'index_menu']);
+    Route::get('/dashboard/menutemuan/{topik_temuan_id}', [\App\Http\Controllers\MenuTemuan::class, 'index']);
+    Route::get('/dashboard/menutemuan/{topik_temuan_id}/menutemuanid/{menu_temuan_id}', [\App\Http\Controllers\MenuTemuan::class, 'index_menu']);
 //deletmenutemuan
-Route::delete('/dashboard/menutemuan/delete/{topik_temuan_id}', [\App\Http\Controllers\MenuTemuan::class, 'destroy']);
+    Route::delete('/dashboard/menutemuan/delete/{topik_temuan_id}', [\App\Http\Controllers\MenuTemuan::class, 'destroy']);
 //addmemutemuan
-Route::get('/dashboard/menutemuan/{topik_temuan_id}/create', [\App\Http\Controllers\MenuTemuan::class, 'create']);
-Route::get('/dashboard/menutemuan/{topik_temuan_id}/create/menutemuanid/{menu_temuan_id}', [\App\Http\Controllers\MenuTemuan::class, 'create']);
+    Route::get('/dashboard/menutemuan/{topik_temuan_id}/create', [\App\Http\Controllers\MenuTemuan::class, 'create']);
+    Route::get('/dashboard/menutemuan/{topik_temuan_id}/create/menutemuanid/{menu_temuan_id}', [\App\Http\Controllers\MenuTemuan::class, 'create']);
 
 //pertanyaantemuan
-Route::get('/dashboard/pertanyaantemuan/{menu_temuan_id}', [\App\Http\Controllers\PertanyaanTemuan::class, 'index']);
-Route::get('/dashboard/pertanyaantemuan/{menu_temuan_id}/create', [\App\Http\Controllers\PertanyaanTemuan::class, 'create']);
+    Route::get('/dashboard/pertanyaantemuan/{menu_temuan_id}', [\App\Http\Controllers\PertanyaanTemuan::class, 'index']);
+    Route::get('/dashboard/pertanyaantemuan/{menu_temuan_id}/create', [\App\Http\Controllers\PertanyaanTemuan::class, 'create']);
 
 //laporan
-Route::get('/dashboard/laporan/front/{topik_temuan_id}', [\App\Http\Controllers\Laporan::class, 'front']);
-Route::get('/dashboard/laporan/front/next/{menu_temuan_id}', [\App\Http\Controllers\Laporan::class, 'next']);
+    Route::get('/dashboard/laporan/front/{topik_temuan_id}', [\App\Http\Controllers\Laporan::class, 'front']);
+    Route::get('/dashboard/laporan/front/next/{menu_temuan_id}', [\App\Http\Controllers\Laporan::class, 'next']);
 
 //pertanyaan
-Route::get('/dashboard/laporan/form/{menu_temuan_id}', [\App\Http\Controllers\Laporan::class, 'form']);
+    Route::get('/dashboard/laporan/form/{menu_temuan_id}', [\App\Http\Controllers\Laporan::class, 'form']);
 
-Route::get('/dashboard/laporan/tampil', [\App\Http\Controllers\Laporan::class, 'tampil']);
-Route::get('/dashboard/laporan/tampil/{id_temuan}', [\App\Http\Controllers\Laporan::class, 'detail']);
+    Route::get('/dashboard/laporan/tampil', [\App\Http\Controllers\Laporan::class, 'tampil']);
+    Route::get('/dashboard/laporan/tampil/{id_temuan}', [\App\Http\Controllers\Laporan::class, 'detail']);
+
+});
